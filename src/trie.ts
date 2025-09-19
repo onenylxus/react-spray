6dcf0f4bfa0948079d0f65ae3fec4c9eee01d820
Nicholas Ng <onenylxus@gmail.com>
2025-09-20 00:17:39 +0800

    Implement base trie structure

--- src/trie.ts
+++ src/trie.ts
@@ -1,301 +1,65 @@
-export interface TrieOptions {
-  inputAttribute: string;
-  outputAttributes: string[];
-  separator: string;
-  forceIndex: boolean;
-  indexPrefix: string;
-  indexSuffix: string;
+class TrieNode<T> {
+  public value: T | null;
+  public children: Map<string, TrieNode<T>>;
+
+  constructor(value: T | null = null) {
+    this.value = value;
+    this.children = new Map();
+  }
 }
 
-export class Trie {
-  public options: TrieOptions;
-  public root: TrieNode;
+export class Trie<T> {
+  private root: TrieNode<T>;
 
-  public constructor(options: Partial<TrieOptions> = {}) {
-    this.options = {
-      inputAttribute: 'data-spray',
-      outputAttributes: ['data-id'],
-      separator: '/',
-      forceIndex: false,
-      indexPrefix: '[',
-      indexSuffix: ']',
-      ...options,
-    };
-    this.root = new TrieNode();
+  public constructor(value?: T) {
+    this.root = new TrieNode<T>(value);
   }
 
-  private get attributeRegex(): RegExp {
-    const prefix = this.options.indexPrefix.replace(
-      /[.*+?^${}()|[\]\\]/g,
-      '\\$&'
-    );
-    const suffix = this.options.indexSuffix.replace(
-      /[.*+?^${}()|[\]\\]/g,
-      '\\$&'
-    );
-    return new RegExp(`^(.+)${prefix}(\\d+)${suffix}$`);
-  }
-
-  private getPath(element: HTMLElement): string[] {
-    const path: string[] = [];
-    let currentElem: HTMLElement | null = element;
-
-    while (currentElem && currentElem.tagName.toLowerCase() !== 'body') {
-      const sprayAttr = currentElem.getAttribute(this.options.inputAttribute);
-      let identifier = sprayAttr || currentElem.tagName.toLowerCase();
-
-      // Find index among siblings with the same identifier
-      if (currentElem.parentElement) {
-        const siblings = Array.from(currentElem.parentElement.children);
-        const sameTypeSiblings = siblings.filter(
-          (sibling) =>
-            (sibling.getAttribute(this.options.inputAttribute) ||
-              sibling.tagName.toLowerCase()) === identifier
-        );
-        if (this.options.forceIndex || sameTypeSiblings.length > 1) {
-          const index = sameTypeSiblings.indexOf(currentElem);
-          identifier = `${identifier}${this.options.indexPrefix}${index}${this.options.indexSuffix}`;
-        }
+  public search(keys: string[]): T | null {
+    let node = this.root;
+    for (const key of keys) {
+      if (!node.children.has(key)) {
+        return null;
       }
-
-      path.unshift(identifier);
-      currentElem = currentElem.parentElement;
+      node = node.children.get(key)!;
     }
-    return path;
+    return node.value;
   }
 
-  private getNodePathFromRoot(root: TrieNode, target: TrieNode): string[] {
-    const path: string[] = [];
-    function dfs(node: TrieNode, currentPath: string[]): boolean {
-      if (node === target) {
-        path.push(...currentPath);
-        return true;
+  public insert(keys: string[], value: T): boolean {
+    let node = this.root;
+    for (const key of keys) {
+      if (!node.children.has(key)) {
+        node.children.set(key, new TrieNode<T>());
       }
-      for (const [key, child] of node.children) {
-        if (dfs(child, [...currentPath, key])) {
-          return true;
-        }
+      node = node.children.get(key)!;
+    }
+    node.value = value;
+    return true;
+  }
+
+  public remove(keys: string[]): boolean {
+    const stack: [string, TrieNode<T>][] = [];
+    let node = this.root;
+
+    for (const key of keys) {
+      if (!node.children.has(key)) {
+        return false;
       }
-      return false;
+      stack.push([key, node]);
+      node = node.children.get(key)!;
     }
-    dfs(root, []);
-    return path;
-  }
+    node.value = null;
 
-  private updateNodeDataId(
-    node: TrieNode,
-    path: string[],
-    base: string = 'body'
-  ) {
-    const fullPath = [base, ...path].join(this.options.separator);
-    if (node.element) {
-      this.options.outputAttributes.forEach((attribute) => {
-        node.element!.setAttribute(attribute, fullPath);
-      });
-    }
-    node.children.forEach((child, key) => {
-      this.updateNodeDataId(child, [...path, key], base);
-    });
-  }
-
-  private updateSiblingIdentifiers(
-    parentNode: TrieNode,
-    parentElem: HTMLElement
-  ) {
-    const baseIdentifiers = new Set<string>();
-    parentNode.children.forEach((_, key) => {
-      const match = key.match(this.attributeRegex);
-      if (match) {
-        baseIdentifiers.add(match[1]);
+    for (let i = stack.length - 1; i >= 0; i--) {
+      const [key, parent] = stack[i];
+      const child = parent.children.get(key)!;
+      if (child.value === null && child.children.size === 0) {
+        parent.children.delete(key);
       } else {
-        baseIdentifiers.add(key);
-      }
-    });
-    baseIdentifiers.forEach((baseIdentifier) => {
-      const siblings = Array.from(parentElem.children).filter(
-        (sibling) =>
-          (sibling.getAttribute(this.options.inputAttribute) ||
-            sibling.tagName.toLowerCase()) === baseIdentifier
-      );
-      if (
-        (this.options.forceIndex && siblings.length === 1) ||
-        siblings.length > 1
-      ) {
-        siblings.forEach((sibling, index) => {
-          const newKey = `${baseIdentifier}${this.options.indexPrefix}${index}${this.options.indexSuffix}`;
-          for (const [oldKey, node] of parentNode.children) {
-            if (node.element === sibling && oldKey !== newKey) {
-              parentNode.children.delete(oldKey);
-              parentNode.children.set(newKey, node);
-              this.updateNodeDataId(
-                node,
-                this.getNodePathFromRoot(this.root, node)
-              );
-              break;
-            }
-          }
-        });
-      } else if (siblings.length === 1) {
-        for (const [oldKey, node] of parentNode.children) {
-          if (node.element === siblings[0] && oldKey !== baseIdentifier) {
-            parentNode.children.delete(oldKey);
-            parentNode.children.set(baseIdentifier, node);
-            this.updateNodeDataId(
-              node,
-              this.getNodePathFromRoot(this.root, node)
-            );
-            break;
-          }
-        }
-      }
-    });
-  }
-
-  public insert(element: HTMLElement | null): void {
-    if (!element) return;
-    let currentNode = this.root;
-    const path = this.getPath(element);
-
-    let currentElem: HTMLElement | null = element;
-    while (currentElem && currentElem.tagName.toLowerCase() !== 'body') {
-      currentElem = currentElem.parentElement;
-    }
-
-    if (currentElem && currentElem.tagName.toLowerCase() === 'body') {
-      let node = currentNode;
-      if (!node.children.has('body')) {
-        node.children.set('body', new TrieNode(document.body));
-      }
-      node = node.children.get('body')!;
-
-      let parentElem = document.body;
-      let nodePath: string[] = [];
-      for (const tagOrSpray of path) {
-        nodePath.push(tagOrSpray);
-        if (!node.children.has(tagOrSpray)) {
-          node.children.set(tagOrSpray, new TrieNode());
-        }
-        node = node.children.get(tagOrSpray)!;
-
-        let baseIdentifier = tagOrSpray;
-        let index: number | null = null;
-        const match = tagOrSpray.match(this.attributeRegex);
-        if (match) {
-          baseIdentifier = match[1];
-          index = parseInt(match[2], 10);
-        }
-        const siblings = Array.from(parentElem.children).filter(
-          (sibling) =>
-            (sibling.getAttribute(this.options.inputAttribute) ||
-              sibling.tagName.toLowerCase()) === baseIdentifier
-        );
-        if (index !== null && siblings[index]) {
-          parentElem = siblings[index] as HTMLElement;
-        } else if (index === null && siblings.length === 1) {
-          parentElem = siblings[0] as HTMLElement;
-        }
-      }
-      node.element = element;
-
-      const fullId = ['body', ...path].join(this.options.separator);
-      this.options.outputAttributes.forEach((attribute) => {
-        element.setAttribute(attribute, fullId);
-      });
-
-      if (parentElem.parentElement) {
-        const parentTrieNode = this.root.children.get('body');
-        if (parentTrieNode) {
-          let parentNode = parentTrieNode;
-          let parentPath = this.getPath(parentElem.parentElement);
-          for (const tagOrSpray of parentPath) {
-            if (parentNode.children.has(tagOrSpray)) {
-              parentNode = parentNode.children.get(tagOrSpray)!;
-            }
-          }
-          this.updateSiblingIdentifiers(parentNode, parentElem.parentElement);
-        }
+        break;
       }
     }
-  }
-
-  public remove(element: HTMLElement | null): void {
-    if (!element) return;
-    let currentNode = this.root;
-    const path = this.getPath(element);
-
-    let currentElem: HTMLElement | null = element;
-    while (currentElem && currentElem.tagName.toLowerCase() !== 'body') {
-      currentElem = currentElem.parentElement;
-    }
-
-    if (currentElem && currentElem.tagName.toLowerCase() === 'body') {
-      let node = currentNode;
-      const stack: [TrieNode, string][] = [];
-
-      if (!node.children.has('body')) return;
-      node = node.children.get('body')!;
-      stack.push([currentNode, 'body']);
-
-      let parentElem = document.body;
-      for (const tagOrSpray of path) {
-        if (!node.children.has(tagOrSpray)) return;
-        stack.push([node, tagOrSpray]);
-        node = node.children.get(tagOrSpray)!;
-
-        let baseIdentifier = tagOrSpray;
-        let index: number | null = null;
-        const match = tagOrSpray.match(this.attributeRegex);
-        if (match) {
-          baseIdentifier = match[1];
-          index = parseInt(match[2], 10);
-        }
-        const siblings = Array.from(parentElem.children).filter(
-          (sibling) =>
-            (sibling.getAttribute(this.options.inputAttribute) ||
-              sibling.tagName.toLowerCase()) === baseIdentifier
-        );
-        if (index !== null && siblings[index]) {
-          parentElem = siblings[index] as HTMLElement;
-        } else if (index === null && siblings.length === 1) {
-          parentElem = siblings[0] as HTMLElement;
-        }
-      }
-
-      node.element = null;
-
-      for (let i = stack.length - 1; i >= 0; i--) {
-        const [parent, key] = stack[i];
-        const child = parent.children.get(key)!;
-        if (child.children.size === 0 && child.element === null) {
-          parent.children.delete(key);
-        } else {
-          break;
-        }
-      }
-
-      if (parentElem.parentElement) {
-        const parentTrieNode = this.root.children.get('body');
-        if (parentTrieNode) {
-          let parentNode = parentTrieNode;
-          let parentPath = this.getPath(parentElem.parentElement);
-          for (const tagOrSpray of parentPath) {
-            if (parentNode.children.has(tagOrSpray)) {
-              parentNode = parentNode.children.get(tagOrSpray)!;
-            }
-          }
-          this.updateSiblingIdentifiers(parentNode, parentElem.parentElement);
-        }
-      }
-    }
-  }
-}
-
-export class TrieNode {
-  public children: Map<string, TrieNode>;
-  public element: HTMLElement | null;
-
-  public constructor(element: HTMLElement | null = null) {
-    this.children = new Map<string, TrieNode>();
-    this.element = element;
+    return true;
   }
 }
---
+++ tests/trie.test.ts
@@ -0,0 +1,143 @@
+import { Trie } from '../src/trie';
+import { randint, randnum, randstr, randstrs } from './fixtures';
+
+describe('constructor', () => {
+  it('Case 1: empty root node', () => {
+    const trie = new Trie<number>();
+    expect(trie).toBeInstanceOf(Trie);
+    expect(trie.search([])).toBeNull();
+  });
+
+  it('Case 2: root node with value', () => {
+    const num = randnum();
+
+    const trie = new Trie<number>(num);
+    expect(trie).toBeInstanceOf(Trie);
+    expect(trie.search([])).toStrictEqual(num);
+  });
+});
+
+describe('search', () => {
+  it('Case 1: valid search', () => {
+    const str = randstr();
+    const obj = { [randstr()]: randnum() };
+
+    const trie = new Trie<Record<string, number>>();
+    trie.insert([str], obj);
+    expect(trie.search([str])).toStrictEqual(obj);
+  });
+
+  it('Case 2: invalid search', () => {
+    const trie = new Trie<unknown>();
+    expect(trie.search([randstr()])).toBeNull();
+  });
+});
+
+describe('insert', () => {
+  it('Case 1: single layer', () => {
+    const str = randstr();
+    const num = randnum();
+
+    const trie = new Trie<number>();
+    const result = trie.insert([str], num);
+    expect(result).toStrictEqual(true);
+    expect(trie.search([str])).toStrictEqual(num);
+  });
+
+  it('Case 2: multiple layers', () => {
+    const strs = randstrs(randint(2, 8));
+    const num = randnum();
+
+    const trie = new Trie<number>();
+    const result = trie.insert(strs, num);
+    expect(result).toStrictEqual(true);
+    for (let i = 1; i < strs.length; i++) {
+      expect(trie.search(strs.slice(0, i))).toBeNull();
+    }
+    expect(trie.search(strs)).toStrictEqual(num);
+  });
+
+  it('Case 3: overwrite', () => {
+    const str = randstr();
+    const num1 = randnum();
+    const num2 = randnum();
+
+    const trie = new Trie<number>();
+    trie.insert([str], num1);
+    const result = trie.insert([str], num2);
+    expect(result).toStrictEqual(true);
+    expect(trie.search([str])).toStrictEqual(num2);
+  });
+
+  it('Case 4: inner overlap', () => {
+    const strs = randstrs(randint(2, 8));
+    const num1 = randnum();
+    const num2 = randnum();
+
+    const trie = new Trie<number>();
+    trie.insert(strs, num1);
+    const result = trie.insert([strs[0]], num2);
+    expect(result).toStrictEqual(true);
+    expect(trie.search([strs[0]])).toStrictEqual(num2);
+    expect(trie.search(strs)).toStrictEqual(num1);
+  });
+
+  it('Case 5: outer overlap', () => {
+    const strs = randstrs(randint(2, 8));
+    const num1 = randnum();
+    const num2 = randnum();
+
+    const trie = new Trie<number>();
+    trie.insert([strs[0]], num1);
+    const result = trie.insert(strs, num2);
+    expect(result).toStrictEqual(true);
+    expect(trie.search(strs)).toStrictEqual(num2);
+    expect(trie.search([strs[0]])).toStrictEqual(num1);
+  });
+});
+
+describe('remove', () => {
+  it('Case 1: simple remove', () => {
+    const str = randstr();
+    const num = randnum();
+
+    const trie = new Trie<number>();
+    trie.insert([str], num);
+    trie.remove([str]);
+    expect(trie.search([str])).toBeNull();
+  });
+
+  it('Case 2: inner overlap', () => {
+    const strs = randstrs(randint(2, 8));
+    const num1 = randnum();
+    const num2 = randnum();
+
+    const trie = new Trie<number>();
+    trie.insert(strs, num1);
+    trie.insert([strs[0]], num2);
+    trie.remove(strs);
+    expect(trie.search(strs)).toBeNull();
+    expect(trie.search([strs[0]])).toStrictEqual(num2);
+  });
+
+  it('Case 3: outer overlap', () => {
+    const strs = randstrs(randint(2, 8));
+    const num1 = randnum();
+    const num2 = randnum();
+
+    const trie = new Trie<number>();
+    trie.insert([strs[0]], num1);
+    trie.insert(strs, num2);
+    trie.remove([strs[0]]);
+    expect(trie.search([strs[0]])).toBeNull();
+    expect(trie.search(strs)).toStrictEqual(num2);
+  });
+
+  it('Case 4: invalid remove', () => {
+    const str = randstr();
+
+    const trie = new Trie<number>();
+    const result = trie.remove([str]);
+    expect(result).toStrictEqual(false);
+  });
+});
